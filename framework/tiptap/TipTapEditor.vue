f1308283008935a41270d04eb591185eca093b0f
wattanx <43837308+wattanx@users.noreply.github.com>
2023-03-17 23:59:43 +0900

    3 files modified, 4 added, 1 deleted

---
+++ framework/tiptap/TipTapEditor.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { Editor, EditorContent } from '@tiptap/vue-2';
+import StarterKit from '@tiptap/starter-kit';
+import { useEditor } from './useEditor';
+
+const editor = useEditor({
+  content: '<p>I’m running Tiptap with Vue.js. 🎉</p>',
+  extensions: [StarterKit],
+});
+</script>
+<template>
+  <EditorContent :editor="editor" />
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,35 +1,10 @@
-<template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        Nuxt
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
-  </div>
-</template>
-
-<script>
-export default {}
+<script setup lang="ts">
+import TipTapEditor from '~/framework/tiptap/TipTapEditor.vue';
 </script>
+
+<template>
+  <TipTapEditor />
+</template>
 
 <style>
 .container {
@@ -42,16 +17,8 @@ export default {}
 }
 
 .title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
   display: block;
   font-weight: 300;
   font-size: 100px;
